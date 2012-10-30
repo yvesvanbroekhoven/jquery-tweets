abe3cd81cd82961aeac4b4570ab377d24bf36366
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2012-10-30 20:21:08 +0100

    Added output & sessionStorage cache

--- jquery-tweets.js
+++ jquery-tweets.js
@@ -2,7 +2,7 @@
  * jquery-tweets
  *
  * Created at: 2012-10-30 16:53:39 +0100
- * Updated at: 2012-10-30 17:31:44 +0100
+ * Updated at: 2012-10-30 20:15:57 +0100
  *
  * Author: @ivow
  * Version: 0.0.0
@@ -20,32 +20,73 @@
   function Tweets( element, url, options ) {
     this.element = element;
     this.url     = url;
+    this.id      = this.url.hashCode();
     this.options = $.extend( {}, defaults, options );
-
+    this.cache   = window.sessionStorage[plugin_name + this.id];
+    console.log(this);
     this.init();
   };
 
   Tweets.prototype.init = function() {
-    $.ajax({
-      dataType: 'jsonp',
-      success: this.output,
-      url: this.url
-    });
+    var _this = this;
+
+    if ( window.JSON && window.Storage && this.cache !== undefined) {
+      console.log( 'it is in the cache yo!' );
+      this.output( $.parseJSON(this.cache) );
+
+    } else {
+      console.log( 'not in the cache yo! ');
+      $.ajax({
+        dataType: 'jsonp',
+        success: function( response_data ){
+          if ( window.JSON && window.Storage ) {
+            window.sessionStorage[plugin_name + _this.id] = JSON.stringify( response_data );
+          }
+          _this.output( response_data );
+        },
+        url: _this.url
+      });
+
+    }
+
   };
 
-  Tweets.prototype.output = function( response_data ) {
-    $.each( response_data, function(idx, value) {
-      console.log( value.text );
+  Tweets.prototype.output = function( data ) {
+    var $ul = $('<ul />');
+    $.each( data, function(idx, value) {
+      var $li = $('<li />');
+      $li.text( value.text );
+      $li.appendTo( $ul );
     });
+    $ul.appendTo( $(this.element) );
   };
 
   $.fn.tweets = function(url, options ) {
-    return this.each(function() {
+    return this.each(function(idx) {
+      console.log(idx);
       if ( !$.data(this, 'plugin_' + plugin_name) ) {
         $.data(this, 'plugin_' + plugin_name, true);
         new Tweets( this, url, options );
       }
     });
+  };
+
+  // Helpers
+  String.prototype.hashCode = function(){
+    var hash = 0,
+        i,
+        character;
+
+    if (this.length == 0) {
+      return hash;
+    }
+
+    for (i = 0; i < this.length; i++) {
+        character = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+character;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return window.Math.abs(hash);
   };
 
 }(jQuery));
--- public/jquery-tweets-min.js
+++ public/jquery-tweets-min.js
@@ -3,10 +3,11 @@
  jquery-tweets
 
  Created at: 2012-10-30 16:53:39 +0100
- Updated at: 2012-10-30 17:31:44 +0100
+ Updated at: 2012-10-30 20:15:57 +0100
 
  Author: @ivow
  Version: 0.0.0
 
 */
-(function(a){function b(c,d,b){this.element=c;this.url=d;this.options=a.extend({},e,b);this.init()}var e={};b.prototype.init=function(){a.ajax({dataType:"jsonp",success:this.output,url:this.url})};b.prototype.output=function(b){a.each(b,function(b,a){console.log(a.text)})};a.fn.tweets=function(c,d){return this.each(function(){a.data(this,"plugin_jquery-tweets")||(a.data(this,"plugin_jquery-tweets",!0),new b(this,c,d))})}})(jQuery);
+(function(c){function d(a,b,f){this.element=a;this.url=b;this.id=this.url.hashCode();this.options=c.extend({},g,f);this.cache=window.sessionStorage[e+this.id];console.log(this);this.init()}var e="jquery-tweets",g={};d.prototype.init=function(){var a=this;window.JSON&&window.Storage&&void 0!==this.cache?(console.log("it is in the cache yo!"),this.output(c.parseJSON(this.cache))):(console.log("not in the cache yo! "),c.ajax({dataType:"jsonp",success:function(b){window.sessionStorage[e+a.id]=JSON.stringify(b);
+a.output(b)},url:a.url}))};d.prototype.output=function(a){var b=c("<ul />");c.each(a,function(a,e){var d=c("<li />");d.text(e.text);d.appendTo(b)});b.appendTo(c(this.element))};c.fn.tweets=function(a,b){return this.each(function(f){console.log(f);c.data(this,"plugin_"+e)||(c.data(this,"plugin_"+e,!0),new d(this,a,b))})};String.prototype.hashCode=function(){var a=0,b,c;if(0==this.length)return a;for(b=0;b<this.length;b++)c=this.charCodeAt(b),a=(a<<5)-a+c,a&=a;return window.Math.abs(a)}})(jQuery);
